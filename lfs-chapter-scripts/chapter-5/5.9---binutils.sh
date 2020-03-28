e3b7d6a86e165293196a3dcde8c300b345063b92
Ahmet <10574489+AhmetOZER@users.noreply.github.com>
2020-03-28 15:22:59 +0300

    build path add for binutils

--- lfs-chapter-scripts/chapter-5/5.9---binutils.sh
+++ lfs-chapter-scripts/chapter-5/5.9---binutils.sh
@@ -4,6 +4,9 @@ cd $LFS/sources
 tar -xvf binutils-2.34.tar.xz
 cd binutils-2.34
 
+mkdir -v build
+cd build
+
 CC=$LFS_TGT-gcc                \
 AR=$LFS_TGT-ar                 \
 RANLIB=$LFS_TGT-ranlib         \
@@ -17,6 +20,10 @@ RANLIB=$LFS_TGT-ranlib         \
 
 make -j$(nproc)
 make install
+
+make -C ld clean
+make -C ld LIB_PATH=/usr/lib:/lib
+cp -v ld/ld-new /tools/bin
 
 cd $LFS/sources
 
